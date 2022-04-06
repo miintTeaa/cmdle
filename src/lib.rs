8e4e1973618f25864555aab9b485467853b1040b
miintTeaa <rebloxminer@gmail.com>
2022-04-06 03:31:04 +0200

    Fixed save write error

--- src/lib.rs
+++ src/lib.rs
@@ -299,10 +299,18 @@ fn save_json(json: JsonValue, path: &str) -> Result<(), &'static str> {
         .create(true)
         .read(false)
         .open(path);
+
     let mut file = match file_result {
         Err(e) => return Err(leak_into_str(format!("Failed to open {}: {:?}", path, e))),
         Ok(file) => file,
     };
+
+    if let Err(e) = file.set_len(0) {
+        return Err(leak_into_str(format!(
+            "Failed to write to {}: {:?}",
+            path, e
+        )));
+    }
 
     if let Err(e) = json.write_pretty(&mut file, 3) {
         return Err(leak_into_str(format!(
@@ -318,7 +326,12 @@ fn load_json(path: &str) -> Result<JsonValue, &'static str> {
     let mut contents = String::new();
     {
         let mut file = match File::open(path) {
-            Err(e) => return Err(leak_into_str(format!("Failed to open {}: {:?}", path, e))),
+            Err(e) => {
+                return Err(leak_into_str(format!(
+                    "Have you ran \"cmdle daily\"? Failed to open {}: {:?}",
+                    path, e
+                )))
+            }
             Ok(file) => file,
         };
         if let Err(e) = file.read_to_string(&mut contents) {
